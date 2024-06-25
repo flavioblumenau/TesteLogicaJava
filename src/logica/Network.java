99cdc20bfce2465da92fddb8a8875e2d6491e21d
flavio.silva <flavio.silva@otimogestor.com.br>
2024-06-25 11:15:37 -0300

    faz a conexao

--- src/logica/Network.java
+++ src/logica/Network.java
@@ -1,5 +1,7 @@
 package logica;
 
+import java.util.Arrays;
+
 // Autor: Flávio da Silva
 // Data: 2024
 
@@ -8,15 +10,18 @@ public class Network {
 	/* a ideia adotada é adicionar um vetor bidimensional 
 	 a dimensão 0 significa a conexao
 	 a dimensão 1 significa os elementos concetados
+	 
 	 Exemplo  conectando os elementos 1 e 6
+	 
 	  arr_connexoes[0][0] = 1;
 	  arr_connexoes[0][1] = 6;
-	  na conexao 0 tem dois elementos 1 e 6
+	  
+	  Signifia que na conexao 0 tem dois elementos: 1 e 6
 	*/
 	
 	private int[][] arr_connexoes = { {}, {} };
-	private int[] arr_numeros = {};
 	
+	private int[] arr_numeros = {};	
 	
 	private boolean conexaoExiste(int a, int b) {
 		// percorre as conexoes e testa se já existe 
@@ -60,15 +65,16 @@ public class Network {
 	 * exceptions as appropriate 
 	 * */
 	public boolean connect(int a, int b) throws Exception {
-		if (! this.numeroExiste(a)) {
-			throw new Exception("Numero " + a + " não encontrado.");
-		}
-		if (! this.numeroExiste(b)) {
-			throw new Exception("Numero " + b + " não encontrado.");
-		}
+		
 		if (this.query(a, b)) {
 			throw new Exception("A conexão entre "+ a + " e " + b + " já existe.");
 		}
+		
+		arr_connexoes = Arrays.copyOf(arr_connexoes, arr_connexoes.length + 1);
+		arr_connexoes[arr_connexoes.length - 1] = new int[2];
+		arr_connexoes[arr_connexoes.length - 1][0] = a;
+		arr_connexoes[arr_connexoes.length - 1][0] = b;
+		
 		
 		return false;
 	}
@@ -79,7 +85,14 @@ public class Network {
 	 * 	It should return true if the elements are connected,
 	 * 	directly or indirectly, and false if the elements are not connected. 
 	 * */
-	public boolean query(int a, int b) {
+	public boolean query(int a, int b) throws Exception {
+		if (! this.numeroExiste(a)) {
+			throw new Exception("Numero " + a + " não encontrado.");
+		}
+		if (! this.numeroExiste(b)) {
+			throw new Exception("Numero " + b + " não encontrado.");
+		}
+		
 		return this.conexaoExiste(a, b);
 	}
 
